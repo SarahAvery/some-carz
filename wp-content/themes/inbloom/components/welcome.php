9072f4687aacb562384b5572d64fbfcde07d2c3a
S Avery <sarahashavery@gmail.com>
2022-07-29 17:57:26 -0400

    welcome img & nav update

--- wp-content/themes/inbloom/components/welcome.php
+++ wp-content/themes/inbloom/components/welcome.php
@@ -9,8 +9,10 @@
           while (have_rows('welcome_images')): the_row();
             $row = get_row(true);
             $image_1 = $row['image_1'];
+            $image_2 = $row['image_2'];
             ?>
-            <div class="banner-img" style="background-image: url(<?php echo $image_1; ?>);"></div>
+            <div class="img img_1" style="background-image: url(<?php echo $image_1; ?>);"></div>
+            <div class="img img_2" style="background-image: url(<?php echo $image_2; ?>);"></div>
             <?php
           endwhile;
         endif;
@@ -32,19 +34,17 @@
           Minus, pariatur? Rerum fugiat veritatis fuga pariatur ducimus dolorem, neque quo quod officia quae!
         </p>
         <p class="para">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore, voluptas possimus dolorem ut unde tempora rem, quae architecto tempore vel cumque eligendi blanditiis iste! Non laborum inventore fugit distinctio maiores?
-        Nemo debitis, inventore nisi mollitia aliquam qui accusamus blanditiis sequi.</p>
+        Nemo debitis, inventore nisi mollitia aliquam qui.</p>
         <p class="para">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore, voluptas possimus dolorem ut unde tempora rem, quae architecto tempore vel cumque eligendi blanditiis iste! Non laborum inventore fugit distinctio maiores?
-        Nemo debitis, inventore nisi mollitia aliquam qui accusamus blanditiis sequi. Rerum fugiat veritatis fuga pariatur ducimus dolorem.</p>
+        Nemo debitis, inventore sequi. Rerum fugiat veritatis fuga pariatur ducimus dolorem.</p>
       </div> 
       <aside>
         <?php
           if (have_rows('welcome_images')):
             while (have_rows('welcome_images')): the_row();
               $row = get_row(true);
-              $image_2 = $row['image_2'];
               $image_3 = $row['image_3'];
               ?>
-              <div class="img img_2" style="background-image: url(<?php echo $image_2; ?>);"></div>
               <div class="img img_3" style="background-image: url(<?php echo $image_3; ?>);"></div>
               <?php
             endwhile;
